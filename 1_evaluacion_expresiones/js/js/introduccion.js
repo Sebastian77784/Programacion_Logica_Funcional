847414d2c637d62a3b6de7f2ec594990250a8383
Sebastian77784 <sebastiannn231@gmail.com>
2025-10-02 14:27:17 -0600

    Programacion Funcional

---
+++ 1_evaluacion_expresiones/js/js/introduccion.js
@@ -0,0 +1,61 @@
+
+let x = [1,2,3,4]
+const y = [...x]
+console.log(y)
+
+const edadLegalEEUU = 21
+const checkEstado = (edad, edadLegal)=>{
+    /*if(edad >= edadLegal){
+        return "Eres mayor";
+    }else{
+        return "Eres menor"
+    }*/
+    return edad >= edadLegal ? "Eres mayor" : "Eres menor"
+}
+const persona =checkEstado (18,edadLegalEEUU)
+console.log(persona)
+
+let arreglo=[
+    {id:1,name:"Sebas", age:20},
+    {id:2,name:"Sebas 3", age:22},
+    {id:3,name:"Sebas 1", age:24},
+    {id:4,name:"Sebas Test", age:25},
+]
+let ul = document.getElementById("lista")
+var todo =""
+arreglo.forEach((item)=>{
+    item.age=item.age * 10 
+    todo += `
+        <li>${item.name} - EDAD: ${item.age}</li>  
+    `
+})
+console.log(todo)
+ul.innerHTML =todo
+
+console.log("Arreglo Original", arreglo);
+var copia = arreglo.map((item,index)=>{
+    item.age *= 10
+    return item
+})
+console.log("Arreglo Modificado", arreglo);
+console.log("Arreglo Modificado", copia);
+
+let url ="https://thesimpsonsapi.com/api/"
+fetch(url+"characters").then(response=>{
+    if(!response.ok){
+        console.log("Error en la red")
+    }
+    return response.json()
+}).then(data=>{
+    console.log("Datos del personaje: ", data)
+    var lis = ""
+    data.results.forEach(personaje=>{
+        lis+=`
+        <li>
+        <img src="https://cdn.thesimpsonsapi.com/500${personaje.portrait_path}" width="200px" />
+        <h2>${personaje.name}</h2>
+        <h2>${personaje.age}</h2>
+        </li>`
+    })
+    ul.innerHTML = lis
+})
